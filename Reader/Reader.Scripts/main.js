9fe21da22260d205c69a2515951d20dada62af43
Miki Riglos <miki.riglos@live.com>
2016-02-11 18:33:32 -0500

    update title with unread quantity

--- Reader/Reader.Scripts/main.js
+++ Reader/Reader.Scripts/main.js
@@ -9,6 +9,17 @@
 define('jquery', function() { return jQuery; });    // already loaded in _layout.cshtml
 
 require(['knockout', 'readerData', 'ViewModels/ReaderViewModel'], function(ko, readerData, ReaderViewModel) {
+    var baseDocumentTitle = document.title;
+    var updateDocumentTitle = function(unreadQuantity) {
+        document.title = baseDocumentTitle + (unreadQuantity ? ' (' + unreadQuantity + ')' : '');
+    };
+
     var readerViewModel = new ReaderViewModel(readerData);
+    // update document title when unread quantity changes
+    var subscriptionAll = readerViewModel.subscriptions()[0];
+    subscriptionAll.unreadQuantity.subscribe(updateDocumentTitle);
+    // ... force 1st time
+    updateDocumentTitle(subscriptionAll.unreadQuantity());
+
     ko.applyBindings(readerViewModel, document.getElementById('readerApp'));
 });
